67d8799d786e067059579f22cdab2448c056f15b
Stefano Vighini <stefano.vighini@gmail.com>
2020-07-29 17:31:43 +0200

    Changed popup display and fixed popup.js bug

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,14 +1,22 @@
 "use strict";
 
 var btnRecord = document.getElementById("btn_record");
+var textRecord = document.getElementById("text_record");
+var imgRecord = document.getElementById("img_record");
 var btnJSON = document.getElementById("btn_json");
 
-chrome.storage.local.get("recording", function(result) {
-	if (result.recording) {
-		change_to_record();
-	}
+// FUNCTIONS
 
-	btnRecord.addEventListener("click", function(event) {
+function change_to_record() {
+	textRecord.innerText = "Stop recording";
+	btnRecord.style.backgroundColor = "red";
+	imgRecord.src = "../icons/stop_record.png"
+}
+
+// LISTENERS
+
+btnRecord.addEventListener("click", function(event) {
+	chrome.storage.local.get("recording", function(result) {
 		if (result.recording) {
 			chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
 				chrome.tabs.sendMessage(tabs[0].id, { stop:true });
@@ -18,12 +26,15 @@ chrome.storage.local.get("recording", function(result) {
 			change_to_record();
 		}
 	});
-	btnJSON.addEventListener("click", function(event) {
-		chrome.tabs.create({ url:"../popup/jsonPattern.html" });
-	});
 });
 
-function change_to_record() {
-	btnRecord.innerText = "Stop recording";
-	btnRecord.style.backgroundColor = "red";
-}
+btnJSON.addEventListener("click", function(event) {
+	chrome.tabs.create({ url:"../popup/jsonPattern.html" });
+});
+
+// CODE
+
+chrome.storage.local.get("recording", function(result) {
+	if (result.recording)
+		change_to_record();
+});
